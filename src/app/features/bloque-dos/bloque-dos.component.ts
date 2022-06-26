303d62de7bab13a95851dd0c1ceaf74f10928b77
Martin Perez <martin.perez.arias@gmail.com>
2022-06-26 15:42:49 -0300

    full-responsive

--- src/app/features/bloque-dos/bloque-dos.component.ts
+++ src/app/features/bloque-dos/bloque-dos.component.ts
@@ -8,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class BloqueDosComponent implements OnInit {
 
   public cardContent = [
-    { title: 'Clientes', body: '+50', icon: 'av_timer' },
-    { title: 'Proyectos', body: '+20', icon: 'energy_program_time_used' },
-    { title: 'Presencia Global', body: '5', icon: 'account_tree' },
-    { title: 'Workshops', body: '+20', icon: 'bolt' },
+    { title: 'Proyectos', body: '+20', icon: 'task' },
+    { title: 'Clientes', body: '+50', icon: 'handshake' },
+    { title: 'Presencia Global', body: '5', icon: 'public' },
+    { title: 'Workshops', body: '+20', icon: 'business' },
   ]
 
   constructor() { }
